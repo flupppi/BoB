870a8640be2d7e1b2b4df163288c6adb8f78601a
Felix Kalchschmid <flupppi@posteo.net>
2024-03-19 15:37:43 +0100

    added Main Game scene and loading to scene

---
+++ Assets/Scripts/Menu Manager.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGameButtonClicked()
+    {
+        SceneManager.LoadScene("Main Game");
+    }
+
+    public void OptionsButtonClicked()
+    {
+        ShowMenu("Options");
+    }
+
+    private void ShowMenu(string v)
+    {
+        Debug.Log("Show Menu not yet implemented");
+    }
+
+    public void ExitButtonClicked()
+    {
+        Application.Quit();
+
+    }
+}
