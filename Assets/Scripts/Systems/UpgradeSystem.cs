12703134f21d23216e5552b88edb69eb05d1b037
Tim Muller <muller.tim@outlook.com>
2024-03-20 18:50:24 +0100

    Upgrades are now unique

--- Assets/Scripts/Systems/UpgradeSystem.cs
+++ Assets/Scripts/Systems/UpgradeSystem.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -34,7 +36,7 @@ public class UpgradeSystem : MonoBehaviour {
         if (m_abilityHolder != null) {
             foreach (AbilityBase currentAbility in m_abilityHolder.Abilities) {
                 if (currentAbility) {
-                    Debug.LogError($"Upgrades: {currentAbility.Upgrades.Length}");
+                    // Debug.LogError($"Upgrades: {currentAbility.Upgrades.Length}");
                     foreach (AbilityBase currentAbilityUpgrade in currentAbility.Upgrades)
                     {
                         if (currentAbilityUpgrade) {
@@ -45,15 +47,48 @@ public class UpgradeSystem : MonoBehaviour {
             }
         }
 
-        Debug.LogError($"AbilityPool: {abilityPool.Count}" );
+        // Debug.LogError($"AbilityPool: {abilityPool.Count}" );
+
+        List<int> randomNumbers = GetNewRandom(abilityPool.Count);
 
         for (int i = 0; i < m_upgradableAbilites.Length; i++) {
             // Eventuell Checken ob kein Index doppelt vorhanden ist
-            int random = Random.Range(0, abilityPool.Count);
-            m_upgradableAbilites[i] = abilityPool[random];
+            //int random =  Random.Range(0, abilityPool.Count);
+            
+            m_upgradableAbilites[i] = abilityPool[randomNumbers[i]];
         }
 
         return m_upgradableAbilites;
+    }
+
+    private List<int> GetNewRandom(int max) {
+        List<int> randomNumbers = new List<int>();
+
+        if (max < 3) {
+            Debug.LogError("Max zu klein");
+            return new List<int>();
+        }
+
+        while (randomNumbers.Count < 3)
+        {
+            int random = Random.Range(0, max);
+            if (!(HasNumber(randomNumbers, random)))
+            {
+                randomNumbers.Add(random);
+            }
+        }
+
+        return randomNumbers;
+    }
+
+    private bool HasNumber(List<int> list, int number) {
+        foreach (int currNumber in list) {
+            if (currNumber == number) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public void SelectAbility(int ability) {
