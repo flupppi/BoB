7d65db07fb2c7f8eef33ee163d8a691b022f9eff
florian1702 <flo-woe@web.de>
2024-03-22 15:18:56 +0100

    Dash fixed

--- Assets/Scripts/Abilities/Dash.cs
+++ Assets/Scripts/Abilities/Dash.cs
@@ -11,14 +11,21 @@ public class Dash : AbilityBase
         behaviour.StartCoroutine(PerformDash(parent));
     }
 
-     private IEnumerator PerformDash(GameObject parent)
+   private IEnumerator PerformDash(GameObject parent)
     {
         CharacterController characterController = parent.GetComponent<CharacterController>();
-        Vector3 dashDirection = parent.transform.forward;
-
+        GameObject upperBody = GameObject.FindGameObjectWithTag("PlayerUpperBody");
+        
         float elapsedTime = 0f;
         while (elapsedTime < activeTime)
         {
+            // Die lokale Blickrichtung des upperBody-Objekts um -90° verringern
+            Vector3 forwardDirection = upperBody.transform.TransformDirection(Vector3.forward);
+            forwardDirection = Quaternion.Euler(0, 90, 0) * forwardDirection;
+
+            // Die Richtung des Dashes entsprechend der aktuellen lokalen Blickrichtung des upperBody-Objekts
+            Vector3 dashDirection = new Vector3(forwardDirection.x, 0f, forwardDirection.z).normalized;
+
             float distanceToMove = dashSpeed * Time.deltaTime;
             characterController.Move(dashDirection * distanceToMove);
             elapsedTime += Time.deltaTime;
