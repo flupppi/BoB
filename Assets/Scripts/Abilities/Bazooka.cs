ae7a9b92283113471c55a827deef9893795645c4
Tim Muller <muller.tim@outlook.com>
2024-03-22 16:27:19 +0100

    Rotated Arena
    Added Bazooka Aimassist

--- Assets/Scripts/Abilities/Bazooka.cs
+++ Assets/Scripts/Abilities/Bazooka.cs
@@ -7,13 +7,41 @@ public class Bazooka : AbilityBase
 {
     [SerializeField] private GameObject bullet;
 
+    private Transform m_aimLocation;
     public override void Activate(GameObject parent) {
         AbilityHolder abilityHolder = parent.GetComponent<AbilityHolder>();
 
         if (!abilityHolder.AbilityLocations[1]) return;
-        GameObject spawnedBullet = Instantiate(bullet, abilityHolder.AbilityLocations[1].position, Quaternion.LookRotation(abilityHolder.AbilityLocations[1].forward) * bullet.transform.localRotation);
+        if (!abilityHolder.AimLocation) return;
+        m_aimLocation = abilityHolder.AimLocation;
+
+        Vector3 shootDirection = GetShootDirection(parent, abilityHolder.AbilityLocations[1]);
+        GameObject spawnedBullet = Instantiate(bullet, abilityHolder.AbilityLocations[1].position, Quaternion.LookRotation(shootDirection) * bullet.transform.localRotation);
 
         Projectile projectileComponent = spawnedBullet.GetComponent<Projectile>();
-        projectileComponent.Direction = abilityHolder.AbilityLocations[1].forward;
+        projectileComponent.Direction = shootDirection;
+    }
+
+    private Vector3 GetShootDirection(GameObject parent, Transform shootLocation)
+    {
+
+        //Debug.DrawRay(m_aimLocation.position, m_aimLocation.forward, Color.blue, 5.0f);
+
+        RaycastHit hit;
+        Vector3 target;
+        Ray ray = new Ray(m_aimLocation.transform.position, m_aimLocation.forward);
+        if (Physics.Raycast(ray, out hit))
+        {
+            target = hit.point;
+        }
+        else
+        {
+            target = ray.GetPoint(1000.0f);
+        }
+
+        Vector3 lookAtDirection = target - shootLocation.position;
+        // Debug.DrawRay(shootLocation.position, lookAtDirection, Color.red, 5.0f);
+
+        return lookAtDirection.normalized;
     }
 }
